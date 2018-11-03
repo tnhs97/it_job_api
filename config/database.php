79c1be0b806ff02a2d09d17b827cfec51ae4fe37
DucVinh2609 <ducvinh2609@gmail.com>
2018-11-03 13:02:46 +0700

    cập nhật cơ sở dữ liệu

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ducvinh09'),
+            'username' => env('DB_USERNAME', 'itjob_database'),
+            'password' => env('DB_PASSWORD', 'ducvinh26091997'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
