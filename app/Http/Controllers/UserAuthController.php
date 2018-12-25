a9bc78b94e549c4b55c1616f901c0a1e39607880
tnhs97 <tnhs97@gmail.com>
2018-12-25 18:50:34 +0700

    jgjg

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -50,10 +50,10 @@ class UserAuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = $request->only(['email', 'password']);
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+        // $credentials = $request->only(['email', 'password']);
+        // if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => $request]);
+        // }
 
         return $this->respondWithToken($token);
     }
